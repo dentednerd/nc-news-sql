ee5d6f63ae9e4d8cd6f5914c09a160f4fab77bc3
Joey Imlay <jo.imlay@gmail.com>
2022-02-12 14:35:38 +0000

    maintenance,m updated README

--- db/connection.js
+++ db/connection.js
@@ -6,10 +6,6 @@ require('dotenv').config({
   path: path.resolve(__dirname, `../.env.${ENV}`),
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
-}
-
 const config = ENV === 'production'
   ? {
       connectionString: process.env.DATABASE_URL,
@@ -18,5 +14,9 @@ const config = ENV === 'production'
       },
     }
   : {};
+
+if (!process.env.PGDATABASE) {
+  throw new Error('PGDATABASE not set');
+}
 
 module.exports = new Pool(config);
